d8cfdad199830d08bf673ee7f8e8c1948a1abb7a
ayz1070 <ayz1070@gmail.com>
2024-12-16 23:21:17 +0900

    :sparkles: [view_model #1] 게시판 ViewModel 생성
    - [x] BoardViewModel

---
+++ lib/features/board/presentation/view_models/board_view_model.dart
@@ -0,0 +1,8 @@
+class BoardViewModel{
+  final String title;
+  final String content;
+  final String imageUrl;
+
+  BoardViewModel({required this.title, required this.content, required this.imageUrl});
+
+}
